93419709a7733bc9c848111b0ccba902b2ce9014
Syyren <129496054+Syyren@users.noreply.github.com>
2024-02-29 02:58:11 -0700

    updated server.js

--- server.js
+++ server.js
@@ -4,24 +4,32 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// MongoDB connection
-mongoose.connect('mongodb+srv://database1:databaseuserpass@cluster0.yurnnop.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+//MongoDB connection
+mongoose.connect('mongodb+srv://database1:databaseuserpass@cluster0.yurnnop.mongodb.net/');
 
-// Check for successful connection
+//checking for successful connection
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
 
-// Middleware
+//listener for connection errors with MongoDB
+mongoose.connection.on('error', (err) => {
+console.error('MongoDB connection error:', err);
+});
+  
+//listener for connection disconnection with MongoDB
+mongoose.connection.on('disconnected', () => {
+console.log('MongoDB disconnected');
+});  
+
+//using the body parser to easily parse JSON
 app.use(bodyParser.json());
 
-// Routes
+//routes the application uses
 app.use('/contacts', require('./routes/ContactRoutes'));
 app.use('/categories', require('./routes/CategoryRoutes'));
 
+//setting the port and checking it's running correctly
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
